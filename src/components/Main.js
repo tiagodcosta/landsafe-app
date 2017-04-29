3c56430b952da6ac01d05ed71bdf096b5956471a
Tiago Costa <doriacosta.t@husky.neu.edu>
2017-04-29 19:01:02 -0400

    Add css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,38 @@
 import React from 'react'
 import { Navbar } from './Navbar'
 import { Footer } from './Footer'
-import Gauge from 'react-svg-gauge';
-import { Geolocation } from './Geolocation'
+import { geolocated } from 'react-geolocated';
+
 import '../scss/index.scss'
 
 export const Main = (props) =>
+	<div>
 	<div className="main-container">
 	<Geolocation />
 	<Navbar />
 		<div className="content-container">
 			{props.children}
 		</div>
+	</div>
 		<Footer />	
 	</div>
+
+
+
+export class Geolocation extends React.Component {
+  render() {
+  	console.log(this.props.coords)
+    return (
+    		<div>
+    			{this.props.coords}
+    		</div>
+    	)
+  }
+}
+
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000
+})(Geolocation);	
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import { Main } from './components/Main'
 import { Home } from './components/Home'
 import { Alert } from './components/Alert'
+import { Details } from './components/Details'
 window.React = React
 
 render(
@@ -11,6 +12,7 @@ render(
 		<Route path="/" component={Main}>
 			<IndexRoute component={Home} />
 			<Route path="alert" component={Alert} />
+			<Route path="details" component={Details} />
 		</Route>
 	</Router>,
 	document.getElementById('main-app')
